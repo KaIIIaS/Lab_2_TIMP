248d296580efac59218b91b2a9d387751d8b3d81
KaIIIaS <145897564+KaIIIaS@users.noreply.github.com>
2023-12-26 10:56:08 +0300

    Update modAlphaCipher.cpp

--- 2/modAlphaCipher.cpp
+++ 2/modAlphaCipher.cpp
@@ -1,55 +1,58 @@
 #include "modAlphaCipher.h"
 
-std::string modAlphaCipher::decrypt(const std::string& cipher_text)              
+string modAlphaCipher::decrypt(const std::string& cipher_text)              
 {
-    std::string text = getValidText(cipher_text);
-    std::string decrypted_text = text;
-    int length, num_of_rows, position, index; 
-    length = text.size();
-    num_of_rows = (length - 1) / num_of_columns + 1;
+    string valid_text = getValidText(cipher_text);
+    string original_text = valid_text;
+    int length, num_columns, position, index; 
+    length = valid_text.size();
+    num_columns = (length - 1) / num_rows + 1;
     index = 0;
-    for (int column_number = num_of_columns; column_number > 0; column_number--) { 
-        for (int row_number = 0; row_number < num_of_rows; row_number++) { 
-            position = num_of_columns * row_number + column_number;
+    for (int column_number = num_columns; column_number > 0; column_number--) { 
+        for (int row_number = 0; row_number < num_rows; row_number++) { 
+            position = num_rows * row_number + column_number;
             if (position - 1 < length) {
-                decrypted_text[position - 1] = text[index];
+                original_text[position - 1] = valid_text[index];
                 index++;
             }
         }
     }
-    return decrypted_text;
+    return original_text;
 }
 
-std::string modAlphaCipher::encrypt(const std::string& text)                     
+string modAlphaCipher::encrypt(const string& original_text)                     
 {
-    std::string open_text = getValidText(text);
-    std::string encrypted_text = open_text;
-    int length, num_of_rows, position, index; 
-    length = open_text.size();
-    num_of_rows = (length - 1) / num_of_columns + 1; 
+    string valid_text = getValidText(original_text);
+    string cipher_text = valid_text;
+    int length, num_columns, position, index; 
+    length = valid_text.size();
+    num_columns = (length - 1) / num_rows + 1; 
     index = 0;
-    for (int column_number = num_of_columns; column_number > 0; column_number--) { 
-        for (int row_number = 0; row_number < num_of_rows; row_number++) {
-            position = column_number + num_of_columns * row_number;
-            if (position - 1 < length) {
-                encrypted_text[index] = open_text[position - 1];
+    for (int column_number = num_columns; column_number > 0; column_number--) { 
+        for (int row_number = 0; row_number < num_rows; row_number++) {
+            position = column_number + num_rows * row_number;
+            if (position-1 < length) {
+                cipher_text[index] = valid_text[position-1];
                 index++;
             }
         }
     }
-    return encrypted_text;
+    return cipher_text;
 }
 
-inline std::string modAlphaCipher::getValidText(const std::string & text)
+inline std::string modAlphaCipher::getValidText(const std::string & str)
 {
-    std::string tmp;
-    for (auto c:text) {
+    string tmp;
+    for (auto c:str) {
         if (isalpha (c)) {
             if (islower (c))
                 tmp.push_back(toupper(c));
-                else
-                    tmp.push_back(c);
+            else
+                tmp.push_back(c);
         }
     }
+
+    if (tmp.empty())
+        throw cipher_error ("Empty open text");
     return tmp;
 }
