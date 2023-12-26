c3773f52923a71bafebd5a514f3031b50abf9905
KaIIIaS <145897564+KaIIIaS@users.noreply.github.com>
2023-12-26 10:56:35 +0300

    Update modAlphaCipher.h

--- 2/modAlphaCipher.h
+++ 2/modAlphaCipher.h
@@ -12,11 +12,11 @@ using namespace std;
 class modAlphaCipher
 {
 private:
-    const int num_of_columns;
+    const int num_rows;
     string getValidText(const std::string & str);
 public:
     modAlphaCipher() = delete;
-    modAlphaCipher(const int& key) :num_of_columns(key) {};    //конструктор
+    modAlphaCipher(const int& key) :num_rows(key) {};    //конструктор
     string encrypt(const string& open_text);               //зашифрование
     string decrypt(const string& cipher_text);             //расшифрование
 };
