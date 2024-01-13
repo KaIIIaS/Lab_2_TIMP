f2a9e941ae0839db66e5f646bbf962a253f2fb7f
KaIIIaS <145897564+KaIIIaS@users.noreply.github.com>
2024-01-13 13:05:58 +0300

    Update modAlphaCipher.cpp

--- 2/modAlphaCipher.cpp
+++ 2/modAlphaCipher.cpp
@@ -1,59 +1,65 @@
+//////////////////////////////////////////
+//		Маслинов						//
+//				Артём					//
+//					Алексеевич			//
+//										//
+//										//
+//										//
+//////////////////////////////////////////
 #include "modAlphaCipher.h"
-
-string modAlphaCipher::decrypt(const std::string& cipher_text)              
+string modAlphaCipher::decrypt(const string& cipher_text) 
 {
-    string valid_text = getValidText(cipher_text);
-    string original_text = valid_text;
-    int length, num_columns, position, index; 
-    length = valid_text.size();
-    num_columns = (length - 1) / num_rows + 1;
-    index = 0;
-    for (int column_number = num_columns; column_number > 0; column_number--) { 
-        for (int row_number = 0; row_number < num_rows; row_number++) { 
-            position = num_rows * row_number + column_number;
-            if (position - 1 < length) {
-                original_text[position - 1] = valid_text[index];
-                index++;
+	string open_text = getValidOpenText(cipher_text);
+    string decrypted_text = open_text;
+    int text_length, position, line, i; 
+    text_length = cipher_text.size(); 
+    line = (text_length - 1) / newkey + 1; 
+    i = 0;
+    for (int column = newkey; column > 0; column--) { 
+        for (int row = 0; row < line; row++) { 
+            position = newkey * row + column; 
+            if (position - 1 < text_length) {
+                decrypted_text[position - 1] = cipher_text[i];
+                i++;
             }
         }
     }
-    return original_text;
+    return decrypted_text;
 }
-
-string modAlphaCipher::encrypt(const string& original_text)                     
+string modAlphaCipher::encrypt(const string& open_text) 
 {
-    string valid_text = getValidText(original_text);
-    string cipher_text = valid_text;
-    int length, num_columns, position, index; 
-    length = valid_text.size();
-    num_columns = (length - 1) / num_rows + 1; 
-    index = 0;
-    for (int column_number = num_columns; column_number > 0; column_number--) { 
-        for (int row_number = 0; row_number < num_rows; row_number++) {
-            position = column_number + num_rows * row_number;
-            if (position-1 < length) {
-                cipher_text[index] = valid_text[position-1];
-                index++;
+	string valid_open_text = getValidOpenText(open_text);
+    string encrypted_text = valid_open_text;
+    int text_length, line, position, i; 
+    text_length = open_text.size(); 
+    line = (text_length - 1) / newkey + 1; 
+    i = 0;
+    for (int column = newkey; column > 0; column--) { 
+        for (int row = 0; row < line; row++) { 
+            position = column + newkey * row;
+            if (position - 1 < text_length) {
+                encrypted_text[i] = open_text[position - 1];
+                i++;
             }
         }
     }
-    
-    
-    return cipher_text;
+    return encrypted_text;
 }
-
-
-string modAlphaCipher::getValidText(const string & text) {
-    if (text.empty()) {
-        throw cipher_error("Пустая строка!");
+inline string modAlphaCipher::getValidOpenText(const string & text)
+{
+	if (text.empty()) {
+        throw cipher_error("Input text cannot be empty");
     }
-    if (static_cast<std::string::size_type>(num_rows) >= text.length()) {
-        throw cipher_error("Длина ключа не может быть больше длины текста!");
+    // Проверка длины ключа
+    if (static_cast<std::string::size_type>(newkey) > text.length()) {
+        throw cipher_error("Invalid key");
     }
+    // Проверка на недопустимые символы
     for (char c : text) {
         if (!isalpha(c)) {
-            throw cipher_error("Текст содержит недопустимые символы!");
+            throw cipher_error("Invalid characters");
         }
     }
+    // Возвращаем длину строки, если все проверки пройдены
     return text;
 }
